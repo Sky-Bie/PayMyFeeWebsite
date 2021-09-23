987406fe67571e73d31cf7f4598a825ca64ee21f
RakeshTShankar <91284370+RakeshTShankar@users.noreply.github.com>
2021-09-24 01:21:18 +0530

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -39,7 +39,7 @@
  <div class="container">
   <div class="row">
    <div class="col-md-12">
-    <nav class="navbar navbar-expand-lg navbar-light reveal fadeInDown"> <a class="navbar-brand" href="index.html">
+    <nav class="navbar navbar-expand-lg navbar-light reveal fadeInDown"> <a class="navbar-brand" href="index.php">
      <img src="images/logo.png" class="img-fluid" alt="logo"></a>
      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
